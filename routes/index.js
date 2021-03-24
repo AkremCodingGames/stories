fa991838ce945334929af9906bf6b4990727ef0e
AkremCodingGames <67330505+AkremCodingGames@users.noreply.github.com>
2021-03-24 19:55:52 +0100

    first step

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+// @desc Login/Landing page
+// @route GET /
+router.get('/', (req, res) => {
+    // res.send('Login')
+    res.render('login', {
+        layout: 'login'
+    })
+})
+
+// @desc Deshboard
+// @route GET /dashboard
+router.get('/dashboard', (req, res) => {
+    // res.send('Dashboard')
+    res.render('dashboard')
+})
+
+module.exports = router
